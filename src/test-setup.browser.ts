6b93fa7f42d8de49cfcdc41273e864c75d5e205a
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2026-06-22 22:11:12 +0100

    First batch of changes

--- src/test-setup.browser.ts
+++ src/test-setup.browser.ts
@@ -3,4 +3,4 @@ import "@analogjs/vitest-angular/setup-snapshots";
 import "@analogjs/vitest-angular/setup-serializers";
 import { setupTestBed } from "@analogjs/vitest-angular/setup-testbed";
 
-setupTestBed({});
+setupTestBed({ browserMode: true });
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,79 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vite';
-import analog from '@analogjs/platform';
-
+import { defineConfig } from "vite";
+import analog from "@analogjs/platform";
+import { playwright } from "@vitest/browser-playwright";
+import { chromaticPlugin } from "@chromatic-com/vitest/plugin";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   build: {
-    target: ['es2020'],
+    target: ["es2020"],
   },
   resolve: {
-    mainFields: ['module'],
+    mainFields: ["module"],
   },
-  plugins: [
-    analog(),
-  ],
+  plugins: [analog()],
   test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['src/test-setup.ts'],
-    include: ['**/*.spec.ts'],
-    reporters: ['default'],
+    projects: [
+      {
+        extends: true,
+        test: {
+          name: "unit",
+          globals: true,
+          environment: "jsdom",
+          setupFiles: ["src/test-setup.ts"],
+          include: ["**/*.spec.unit.ts"],
+          reporters: ["default"],
+        },
+      },
+      {
+        extends: true,
+        test: {
+          name: "non-chromatic",
+          globals: true,
+          setupFiles: ["src/test-setup.browser.ts"],
+          include: ["src/**/*.spec.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+          reporters: ["default"],
+          browser: {
+            enabled: true,
+            headless: false, // set to true in CI
+            provider: playwright(),
+            instances: [{ browser: "chromium" }],
+          },
+        },
+      },
+      {
+        extends: true,
+        plugins: [
+          chromaticPlugin({
+            disableAutoSnapshot: true,
+            reporter: {
+              enabled: true,
+              verbose: true,
+            },
+          }),
+        ],
+        test: {
+          name: "chromatic",
+          include: ["src/**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+          setupFiles: ["src/test-setup.browser.ts"],
+          browser: {
+            enabled: true,
+            headless: true,
+            screenshotFailures: false,
+            provider: playwright({}),
+            viewport: {
+              width: 1280,
+              height: 800,
+            },
+            instances: [
+              { browser: "chromium" },
+              /*   { browser: "firefox" },
+              { browser: "webkit" }, */
+            ],
+          },
+        },
+      },
+    ],
   },
 }));
